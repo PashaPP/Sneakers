7b107f3b27999bb1f0b3ebd6d2f9d23d56c3b499
Pavel Petrov <2087515@gmail.com>
2022-08-27 17:06:21 +0700

    scss

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,20 @@
 function App() {
-  return <div className="App">SNEAKERS</div>;
+  return (
+    <div className="wrapper">
+      <div className="container">
+        <div className="header">
+          <div className="header__content">
+            <div className="header__title">REACT SNEAKERS</div>
+            <h2>Магазин лучших кроссовок</h2>
+          </div>
+        </div>
+        <div className="reclama"></div>
+        <div className="main">
+          <div className="main__title">Все кроссовки</div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
