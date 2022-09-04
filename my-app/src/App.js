9a7ae3097f515e17dfb2f856c80c0aff67e4331c
Pavel Petrov <2087515@gmail.com>
2022-09-04 18:50:44 +0700

    Before - react

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,7 +3,9 @@ function App() {
     <div className="wrapper clear">
       <div className="overlay">
         <div className="drawer">
-          <h2>Корзина</h2>
+          <h2 className="d-flex justify-between ">
+            Корзина <img className="removeBtn cu-p" src="/img/btn-remove.svg" alt="Plus" />
+          </h2>
           <div className="items">
             <div className="cartItem d-flex align-center mb-20">
               <div
@@ -27,19 +29,47 @@ function App() {
               </div>
               <img className="removeBtn" src="/img/btn-remove.svg" alt="Plus" />
             </div>
+            <div className="cartItem d-flex align-center mb-20">
+              <div
+                style={{ backgroundImage: 'url(/img/sneakers/3.jpg' }}
+                className="cartItemImg"></div>
+
+              <div className="mr-20">
+                <p className="mb-5">Мужские Кроссовки Nike Air Max 270</p>
+                <b>12 999 руб.</b>
+              </div>
+              <img className="removeBtn" src="/img/btn-remove.svg" alt="Plus" />
+            </div>
+            <div className="cartItem d-flex align-center mb-20">
+              <div
+                style={{ backgroundImage: 'url(/img/sneakers/4.jpg' }}
+                className="cartItemImg"></div>
+
+              <div className="mr-20">
+                <p className="mb-5">Мужские Кроссовки Nike Air Max 270</p>
+                <b>12 999 руб.</b>
+              </div>
+              <img className="removeBtn" src="/img/btn-remove.svg" alt="Plus" />
+            </div>
           </div>
-          <ul className="cartTotalBlock">
-            <li className="d-flex">
-              <span>Итого: </span>
-              <div></div>
-              <b>21 498 руб. </b>
-            </li>
-            <li className="d-flex">
-              <span>Налог 5%: </span>
-              <div></div>
-              <b>1074 руб. </b>
-            </li>
-          </ul>
+          <div className="cartTotalBlock">
+            <ul>
+              <li className="d-flex">
+                <span>Итого: </span>
+                <div></div>
+                <b>21 498 руб. </b>
+              </li>
+              <li className="d-flex">
+                <span>Налог 5%: </span>
+                <div></div>
+                <b>1074 руб. </b>
+              </li>
+            </ul>
+            <button className="greenButton">
+              Оформить заказ
+              <img src="/img/arrow.svg" alt="arrow" />
+            </button>
+          </div>
         </div>
       </div>
 
